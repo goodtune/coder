ed8f526790d74c2d03a5210b4d3a423f8a05ed40
McKayla Washburn <mckayla@hey.com>
2024-02-15 20:26:46 +0000

    `make gen`

--- site/src/api/typesGenerated.ts
+++ site/src/api/typesGenerated.ts
@@ -2117,11 +2117,10 @@ export const ResourceTypes: ResourceType[] = [
 ];
 
 // From codersdk/serversentevents.go
-export type ServerSentEventType = "data" | "error" | "partial" | "ping";
+export type ServerSentEventType = "data" | "error" | "ping";
 export const ServerSentEventTypes: ServerSentEventType[] = [
   "data",
   "error",
-  "partial",
   "ping",
 ];
 
